578bbe69f6327b6ac03fdd578a47c6a925f0c175
marshall-strong <marshallstrong123@gmail.com>
2021-07-02 21:02:59 -0400

    install annotate gem to add schema information to models
    `$ bundle install`
    `$ bundle exec rails g annotate:install`
    `Running via Spring preloader in process 39908
          create  lib/tasks/auto_annotate_models.rake`
    `$ rails annotate_models`

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  description :string
+#  ext_prc     :string
+#  order_qty   :string
+#  part_no     :string
+#  ship_qty    :string
+#  tax         :string
+#  unit_price  :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
 end
