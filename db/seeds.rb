a1892617a4f03d128dcb188378fbf417fd177691
marshall-strong <marshallstrong123@gmail.com>
2021-07-06 19:01:16 -0400

    add associations to seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,31 +23,49 @@ food_barn = Building.create!(name: "Food Barn")
 corn_barn = Building.create!(name: "Corn Barn")
 shade_barn = Building.create!(name: "Shade Barn")
 csa_barn = Building.create!(name: "CSA Barn")
-shakeup_truck = Building.create!(name: "Shakeup Truck")
-smoke_shack = Building.create!(name: "Smoke Shack")
-grill_shack = Building.create!(name: "Grill Shack")
+shakeup_truck_building = Building.create!(name: "Shakeup Truck")
+smoke_shack_building = Building.create!(name: "Smoke Shack")
+grill_shack_building = Building.create!(name: "Grill Shack")
 
 # Kitchens
 bakery = Kitchen.create!(name: "Bakery")
+bakery.building = main_barn
 smokehouse = Kitchen.create!(name: "Smokehouse")
+smokehouse.building = main_barn
 shakes_and_cakes = Kitchen.create!(name: "Shakes & Cakes")
+shakes_and_cakes.building = food_barn
 pizza_porch = Kitchen.create!(name: "Pizza Porch")
+pizza_porch.building = food_barn
 fresh_grill = Kitchen.create!(name: "Fresh Grill")
+fresh_grill.building = food_barn
 kettle_barn = Kitchen.create!(name: "Kettle Barn")
+kettle_barn.building = corn_barn
 shakeup_truck_kitchen = Kitchen.create!(name: "Shakeup Truck")
+shakeup_truck_kitchen.building = shakeup_truck_building
 smoke_shack_kitchen = Kitchen.create!(name: "Smoke Shack")
+smoke_shack_kitchen.building = smoke_shack_building
 grill_shack_kitchen = Kitchen.create!(name: "Grill Shack")
+grill_shack_kitchen.building = grill_shack_building
 
 # Recipes 
-cheese_pizza = Recipe.create!(name: "Personal Cheese Pizza", directions: "Here is how to make a Personal Cheese Pizza...")
+cheese_pizza_recipe = Recipe.create!(name: "Personal Cheese Pizza", directions: "Here is how to make a Personal Cheese Pizza...")
+cheese_pizza_recipe.prep_kitchen = pizza_porch
 
 # Paper Products
 box_for_cheese_pizza = PaperProduct.create!(name: "pizza box for Cheese Pizza")
+box_for_cheese_pizza.product = regional_pizza_boxes
+box_for_cheese_pizza.recipe = cheese_pizza_recipe
 
 # Food Products
 cheese_pizza_crust = FoodProduct.create!(name: "pizza crust")
+cheese_pizza_crust.product = regional_pizza_crusts
+cheese_pizza_crust.recipe = cheese_pizza_recipe
 cheese_pizza_sauce = FoodProduct.create!(name: "pizza sauce")
+cheese_pizza_sauce.product = regional_pizza_sauce
+cheese_pizza_sauce.recipe = cheese_pizza_recipe
 cheese_pizza_cheese = FoodProduct.create!(name: "mozzarella cheese")
+cheese_pizza_cheese.product = regional_mozzarella_cheese
+cheese_pizza_cheese.recipe = cheese_pizza_recipe
 
 # NCRs
 
