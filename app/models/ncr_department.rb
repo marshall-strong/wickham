4c88ed49e716d01c0d7ed8e398f886077a516337
marshall-strong <marshallstrong123@gmail.com>
2021-07-07 15:07:20 -0400

    each NCR Item belongs to a Category, and each NCR Category belongs to a Department

--- app/models/ncr_department.rb
+++ app/models/ncr_department.rb
@@ -1,2 +1,3 @@
 class NcrDepartment < ApplicationRecord
+  has_many :ncr_categories, class_name: :NcrCategory, foreign_key: "ncr_department_id"
 end
--- app/models/ncr_item.rb
+++ app/models/ncr_item.rb
@@ -1,2 +1,3 @@
 class NcrItem < ApplicationRecord
+  belongs_to :ncr_category, class_name: "ncr_category", foreign_key: "ncr_category_id"
 end
