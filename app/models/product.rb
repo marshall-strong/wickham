838ae1803e4cd07fd17c86da3cc56fd53e928acb
marshall-strong <marshallstrong123@gmail.com>
2021-07-06 20:10:15 -0400

    add through associations to the Product model -- Product.recipes will return all of the recipes that a product (food or paper) is being used in

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,4 +13,9 @@ class Product < ApplicationRecord
   belongs_to :vendor, class_name: :Vendor, foreign_key: "vendor_id"
   has_many :paper_products, class_name: :PaperProduct, foreign_key: "product_id"
   has_many :food_products, class_name: :FoodProduct, foreign_key: "product_id"
+  has_many :paper_recipes, through: :paper_products, source: :recipe
+  has_many :food_recipes, through: :food_products, source: :recipe
+  def recipes
+    self.paper_recipes + self.food_recipes
+  end
 end
