00860a4c85c0ff3b7e0a4fb0455ad308bef2d5af
marshall-strong <marshallstrong123@gmail.com>
2021-07-06 20:07:52 -0400

    add through associations to the Kitchen model -- Kitchen.recipes, Kitchen.paper_products, Kitchen.food_products

--- app/models/kitchen.rb
+++ app/models/kitchen.rb
@@ -11,4 +11,6 @@
 class Kitchen < ApplicationRecord
   belongs_to :building, class_name: :Building, foreign_key: "building_id"
   has_many :recipes, class_name: :Recipe, foreign_key: "prep_kitchen_id"
+  has_many :paper_products, through: :recipes, source: :paper_products
+  has_many :food_products, through: :recipes, source: :ingredients
 end
