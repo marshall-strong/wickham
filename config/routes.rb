4d56c9bbcc0a50d209c798c21328d663965bc873
marshall-strong <marshallstrong123@gmail.com>
2021-07-13 09:06:08 -0400

    update routes -- `api/v1/buildings` routes to the `index` method of the `BuildingsController` (which renders `views/api/v1/index.json.jbuilder`

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,29 @@
 Rails.application.routes.draw do
-  resources :ncr_items
-  resources :ncr_categories
-  resources :ncr_departments
-  resources :buildings
-  resources :kitchens
-  resources :recipes
-  resources :ncrs
-  resources :vendors
-  resources :products
-  resources :food_products
-  resources :paper_products
-  resources :invoices
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      get '/buildings', to: 'buildings#index'
+    end
+  end
+
+  # # if no routes match the request, fallback to returning client/public/index.html
+  # # This should fix the error that happen when the frontend route is somehow
+  # #  sent to the backend, causing an error like "no route matches /albums/undefined"
+  # get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+  #   !request.xhr? && request.format.html?
+  # end
+
+  # resources :ncr_items
+  # resources :ncr_categories
+  # resources :ncr_departments
+  # resources :buildings
+  # resources :kitchens
+  # resources :recipes
+  # resources :ncrs
+  # resources :vendors
+  # resources :products
+  # resources :food_products
+  # resources :paper_products
+  # resources :invoices
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
