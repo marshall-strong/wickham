39cec4d99cffed816ddf2fd5ec38473c61cac82a
marshall-strong <marshallstrong123@gmail.com>
2021-07-13 13:30:54 -0400

    Create `Recipes.jsx` using `Buildings.jsx` as a template (created using Find & Replace). It exports 3 components: `<Recipe/>`, `<Recipes/>`, and the default export, `<RecipesContainer/>`. `RecipeContainer` initiates the async requests, and the status of the requests are tracked in the recipes slice-of-state.

--- client/src/features/recipes/Recipes.jsx
+++ client/src/features/recipes/Recipes.jsx
@@ -1,20 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
-  fetchBuildings,
-  selectBuildingById,
-  selectBuildingIds,
+  fetchRecipes,
+  selectRecipeById,
+  selectRecipeIds,
 } from "./recipesSlice";
 import List from "../lists/List";
 
-export const Building = ({ buildingId }) => {
-  const building = useSelector((state) =>
-    selectBuildingById(state, buildingId)
-  );
+export const Recipe = ({ recipeId }) => {
+  const recipe = useSelector((state) => selectRecipeById(state, recipeId));
   return (
     <article className="p-4 flex space-x-4">
       <img
-        // src={building.image}
+        // src={recipe.image}
         alt=""
         className="flex-none w-18 h-18 rounded-lg object-cover bg-gray-100"
         width="144"
@@ -22,18 +20,18 @@ export const Building = ({ buildingId }) => {
       />
       <div className="min-w-0 relative flex-auto sm:pr-20 lg:pr-0 xl:pr-20">
         <h2 className="text-lg font-semibold text-black mb-0.5">
-          {building.name}
+          {recipe.name}
         </h2>
         <dl className="flex flex-wrap text-sm font-medium whitespace-pre">
           <div>
             <dt className="sr-only">id</dt>
             <dd>
-              <abbr title={`id: ${building.id}`}>id: {building.id}</abbr>
+              <abbr title={`id: ${recipe.id}`}>id: {recipe.id}</abbr>
             </dd>
           </div>
           <div>
             <dt className="sr-only">id</dt>
-            <dd> · {building.id}</dd>
+            <dd> · {recipe.id}</dd>
           </div>
           <div className="flex-none w-full mt-0.5 font-normal">
             <dt className="inline">Part of </dt>{" "}
@@ -45,45 +43,45 @@ export const Building = ({ buildingId }) => {
   );
 };
 
-export const Buildings = ({ buildingIds }) => {
+export const Recipes = ({ recipeIds }) => {
   return (
     <List>
-      {buildingIds.map((id) => (
-        <Building key={id} buildingId={id} />
+      {recipeIds.map((id) => (
+        <Recipe key={id} recipeId={id} />
       ))}
     </List>
   );
 };
 
-const BuildingsContainer = () => {
+const RecipesContainer = () => {
   const dispatch = useDispatch();
   const [requestSent, setRequestSent] = useState(false);
   useEffect(() => {
     if (!requestSent) {
-      dispatch(fetchBuildings());
+      dispatch(fetchRecipes());
       setRequestSent(true);
     }
   }, [requestSent, dispatch]);
 
-  const fetchBuildingsStatus = useSelector(
-    (state) => state.buildings.status.fetchBuildings
+  const fetchRecipesStatus = useSelector(
+    (state) => state.recipes.status.fetchRecipes
   );
-  const allBuildingIds = useSelector((state) => selectBuildingIds(state));
+  const allRecipeIds = useSelector((state) => selectRecipeIds(state));
   let content;
 
-  if (!fetchBuildingsStatus || fetchBuildingsStatus === "pending") {
+  if (!fetchRecipesStatus || fetchRecipesStatus === "pending") {
     content = <div className="loader" />;
-  } else if (fetchBuildingsStatus === "fulfilled") {
-    content = <Buildings buildingIds={allBuildingIds} />;
-  } else if (fetchBuildingsStatus === "rejected") {
-    content = <div>fetchBuildings was rejected!!!</div>;
+  } else if (fetchRecipesStatus === "fulfilled") {
+    content = <Recipes recipeIds={allRecipeIds} />;
+  } else if (fetchRecipesStatus === "rejected") {
+    content = <div>fetchRecipes was rejected!!!</div>;
   } else {
     content = (
-      <div>Something unexpected happened in the BuildingsContainer...</div>
+      <div>Something unexpected happened in the RecipesContainer...</div>
     );
   }
 
-  return <div className="Buildings">{content}</div>;
+  return <div className="Recipes">{content}</div>;
 };
 
-export default BuildingsContainer;
+export default RecipesContainer;
