2cecfafaf7f7b42e995950b74261c6830ccf7f4f
marshall-strong <marshallstrong123@gmail.com>
2021-07-13 12:11:38 -0400

    remove unused selector function from `Buildings` imports

--- client/src/features/buildings/Buildings.jsx
+++ client/src/features/buildings/Buildings.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   fetchBuildings,
-  selectAllBuildings,
   selectBuildingById,
   selectBuildingIds,
 } from "./buildingsSlice";
