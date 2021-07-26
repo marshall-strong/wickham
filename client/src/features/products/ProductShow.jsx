d85eeaa1e19c8b118ee1dd149d29f865b8545672
marshall-strong <marshallstrong123@gmail.com>
2021-07-26 14:07:40 -0400

    update the index page and the show pages for the `products` feature

--- client/src/features/products/ProductShow.jsx
+++ client/src/features/products/ProductShow.jsx
@@ -1,1 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProduct } from "./productsSlice";
+import ProductShowItem from "./ProductShowItem";
+
+const ProductShow = ({ match }) => {
+  const productId = parseInt(match.params.product_id);
+  const dispatch = useDispatch();
+  const [requestSent, setRequestSent] = useState(false);
+  useEffect(() => {
+    if (!requestSent) {
+      dispatch(fetchProduct(productId));
+      setRequestSent(true);
+    }
+  }, [requestSent, dispatch]);
+
+  const fetchProductStatus = useSelector(
+    (state) => state.products.status.fetchProduct
+  );
+
+  let content;
+  if (!fetchProductStatus || fetchProductStatus === "pending") {
+    content = <div className="loader" />;
+  } else if (fetchProductStatus === "fulfilled") {
+    content = <ProductShowItem productId={productId} />;
+  } else if (fetchProductStatus === "rejected") {
+    content = (
+      <div>
+        <p>Error in the `ProductShow` component:</p>
+        <p>the request `fetchProduct` was rejected!</p>
+      </div>
+    );
+  } else {
+    content = (
+      <div>Something unexpected happened in the ProductShow component...</div>
+    );
+  }
+
+  return <div className="ProductShow">{content}</div>;
+};
+
+export default ProductShow;
---
+++ client/src/features/products/ProductShowItem.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectProductById } from "./productsSlice";
+
+const ProductShowItem = ({ productId }) => {
+  const product = useSelector((state) => selectProductById(state, productId));
+  return (
+    <article className="Product p-4 flex space-x-4">
+      <img
+        // src={product.image}
+        alt=""
+        className="flex-none w-18 h-18 rounded-lg object-cover bg-gray-100"
+        width="144"
+        height="144"
+      />
+      <div className="min-w-0 relative flex-auto sm:pr-20 lg:pr-0 xl:pr-20">
+        <h2 className="text-lg font-semibold text-black mb-0.5">
+          {product.name}
+        </h2>
+        <dl>
+          <div className="flex-none w-full mt-0.5 font-normal">
+            <dt className="inline">id</dt>
+            {": "}
+            <dd className="inline text-black">{product.id}</dd>
+          </div>
+          <div className="flex-none w-full mt-0.5 font-normal">
+            <dt className="inline">name</dt>
+            {": "}
+            <dd className="inline text-black">{product.name}</dd>
+          </div>
+        </dl>
+      </div>
+    </article>
+  );
+};
+
+export default ProductShowItem;
