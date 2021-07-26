16b56ee0f9f217c874444c1c54719de50967065f
marshall-strong <marshallstrong123@gmail.com>
2021-07-26 14:07:03 -0400

    update the index page and the show pages for the `paperProducts` feature -- update the jBuilder view for show to use `paperProducts` instead of `paper_products` as the top-level key (must match the Redux store)

--- client/src/features/paperProducts/PaperProductShow.jsx
+++ client/src/features/paperProducts/PaperProductShow.jsx
@@ -1,1 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPaperProduct } from "./paperProductsSlice";
+import PaperProductShowItem from "./PaperProductShowItem";
+
+const PaperProductShow = ({ match }) => {
+  const paperProductId = parseInt(match.params.paper_product_id);
+  const dispatch = useDispatch();
+  const [requestSent, setRequestSent] = useState(false);
+  useEffect(() => {
+    if (!requestSent) {
+      dispatch(fetchPaperProduct(paperProductId));
+      setRequestSent(true);
+    }
+  }, [requestSent, dispatch]);
+
+  const fetchPaperProductStatus = useSelector(
+    (state) => state.paperProducts.status.fetchPaperProduct
+  );
+
+  let content;
+  if (!fetchPaperProductStatus || fetchPaperProductStatus === "pending") {
+    content = <div className="loader" />;
+  } else if (fetchPaperProductStatus === "fulfilled") {
+    content = <PaperProductShowItem paperProductId={paperProductId} />;
+  } else if (fetchPaperProductStatus === "rejected") {
+    content = (
+      <div>
+        <p>Error in the `PaperProductShow` component:</p>
+        <p>the request `fetchPaperProduct` was rejected!</p>
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        Something unexpected happened in the PaperProductShow component...
+      </div>
+    );
+  }
+
+  return <div className="PaperProductShow">{content}</div>;
+};
+
+export default PaperProductShow;
--- client/src/features/paperProducts/PaperProductsIndexItem.jsx
+++ client/src/features/paperProducts/PaperProductsIndexItem.jsx
@@ -1,1 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectPaperProductById } from "./paperProductsSlice";
+
+const PaperProductsIndexItem = ({ paperProductId }) => {
+  const paperProduct = useSelector((state) =>
+    selectPaperProductById(state, paperProductId)
+  );
+  return (
+    <article className="p-4 flex space-x-4">
+      <Link to={`/paper_products/${paperProductId}`}>
+        <img
+          // src={paperProduct.image}
+          alt=""
+          className="flex-none w-18 h-18 rounded-lg object-cover bg-gray-100"
+          width="144"
+          height="144"
+        />
+        <div className="min-w-0 relative flex-auto sm:pr-20 lg:pr-0 xl:pr-20">
+          <h2 className="text-lg font-semibold text-black mb-0.5">
+            {paperProduct.name}
+          </h2>
+          <dl>
+            <div className="flex-none w-full mt-0.5 font-normal">
+              <dt className="inline">id</dt>
+              {": "}
+              <dd className="inline text-black">{paperProduct.id}</dd>
+            </div>
+            <div className="flex-none w-full mt-0.5 font-normal">
+              <dt className="inline">name</dt>
+              {": "}
+              <dd className="inline text-black">{paperProduct.name}</dd>
+            </div>
+          </dl>
+        </div>
+      </Link>
+    </article>
+  );
+};
+
+export default PaperProductsIndexItem;
