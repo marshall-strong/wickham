b72eacf5115a986b2e513472a82053f146a781c9
marshall-strong <marshallstrong123@gmail.com>
2021-07-26 14:09:18 -0400

    Clean up comments in `routes.rb`

--- config/routes.rb
+++ config/routes.rb
@@ -40,14 +40,4 @@ Rails.application.routes.draw do
   #   !request.xhr? && request.format.html?
   # end
 
-
-  # resources :buildings
-  # resources :kitchens
-  # resources :recipes
-  # resources :vendors
-  # resources :products
-  # resources :food_products
-  # resources :paper_products
-  # resources :invoices
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
