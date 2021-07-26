751dd1f0d13b370f59028e9891008d58869401d8
marshall-strong <marshallstrong123@gmail.com>
2021-07-26 11:43:34 -0400

    update NavBar to use Link elements

--- client/src/app/nav/NavBar.jsx
+++ client/src/app/nav/NavBar.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // the NavItem component is where styling for the individual links is applied
-const NavItem = ({ href, isActive, children }) => {
+const NavItem = ({ link, isActive, children }) => {
   return (
     <li
       className={`block px-4 py-2 rounded-md ${
         isActive ? "bg-red-100 text-blue-700" : ""
       }`}
     >
-      <a href={href}>{children}</a>
+      <Link to={link}>{children}</Link>
     </li>
   );
 };
@@ -34,28 +35,28 @@ const NavBar = () => {
           GitHub
         </a>
       </li>
-      <NavItem href="/" isActive={true}>
+      <NavItem link="/" isActive={true}>
         Home
       </NavItem>
-      <NavItem href="/buildings" isActive={false}>
+      <NavItem link="/buildings" isActive={false}>
         Buildings
       </NavItem>
-      <NavItem href="/kitchens" isActive={false}>
+      <NavItem link="/kitchens" isActive={false}>
         Kitchens
       </NavItem>
-      <NavItem href="/products" isActive={false}>
+      <NavItem link="/products" isActive={false}>
         Products
       </NavItem>
-      <NavItem href="/recipes" isActive={false}>
+      <NavItem link="/recipes" isActive={false}>
         Recipes
       </NavItem>
-      <NavItem href="/paper_products" isActive={false}>
+      <NavItem link="/paper_products" isActive={false}>
         Paper Products
       </NavItem>
-      <NavItem href="/food_products" isActive={false}>
+      <NavItem link="/food_products" isActive={false}>
         Food Products
       </NavItem>
-      <NavItem href="/vendors" isActive={false}>
+      <NavItem link="/vendors" isActive={false}>
         Vendors
       </NavItem>
     </NavList>
