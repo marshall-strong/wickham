385cd0b722727f4d639112ec080f882acbe38d08
marshall-strong <marshallstrong123@gmail.com>
2021-07-26 14:05:29 -0400

    update the index page and the show pages for the `invoices` feature

--- client/src/features/invoices/InvoiceShow.jsx
+++ client/src/features/invoices/InvoiceShow.jsx
@@ -1,1 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchInvoice } from "./invoicesSlice";
+import InvoiceShowItem from "./InvoiceShowItem";
+
+const InvoiceShow = ({ match }) => {
+  const invoiceId = parseInt(match.params.invoice_id);
+  const dispatch = useDispatch();
+  const [requestSent, setRequestSent] = useState(false);
+  useEffect(() => {
+    if (!requestSent) {
+      dispatch(fetchInvoice(invoiceId));
+      setRequestSent(true);
+    }
+  }, [requestSent, dispatch]);
+
+  const fetchInvoiceStatus = useSelector(
+    (state) => state.invoices.status.fetchInvoice
+  );
+
+  let content;
+  if (!fetchInvoiceStatus || fetchInvoiceStatus === "pending") {
+    content = <div className="loader" />;
+  } else if (fetchInvoiceStatus === "fulfilled") {
+    content = <InvoiceShowItem invoiceId={invoiceId} />;
+  } else if (fetchInvoiceStatus === "rejected") {
+    content = (
+      <div>
+        <p>Error in the `InvoiceShow` component:</p>
+        <p>the request `fetchInvoice` was rejected!</p>
+      </div>
+    );
+  } else {
+    content = (
+      <div>Something unexpected happened in the InvoiceShow component...</div>
+    );
+  }
+
+  return <div className="InvoiceShow">{content}</div>;
+};
+
+export default InvoiceShow;
--- client/src/features/invoices/InvoicesIndex.jsx
+++ client/src/features/invoices/InvoicesIndex.jsx
@@ -1,1 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchInvoices, selectInvoiceIds } from "./invoicesSlice";
+import List from "../../app/lists/List";
+import InvoicesIndexItem from "./InvoicesIndexItem";
+
+const InvoicesIndexItems = ({ invoiceIds }) => {
+  return (
+    <div className="Invoices">
+      <List>
+        {invoiceIds.map((id) => (
+          <InvoicesIndexItem key={id} invoiceId={id} />
+        ))}
+      </List>
+    </div>
+  );
+};
+
+const InvoicesIndex = () => {
+  const dispatch = useDispatch();
+  const [requestSent, setRequestSent] = useState(false);
+  useEffect(() => {
+    if (!requestSent) {
+      dispatch(fetchInvoices());
+      setRequestSent(true);
+    }
+  }, [requestSent, dispatch]);
+
+  const fetchInvoicesStatus = useSelector(
+    (state) => state.invoices.status.fetchInvoices
+  );
+  const allInvoiceIds = useSelector((state) => selectInvoiceIds(state));
+  let content;
+
+  if (!fetchInvoicesStatus || fetchInvoicesStatus === "pending") {
+    content = <div className="loader" />;
+  } else if (fetchInvoicesStatus === "fulfilled") {
+    content = <InvoicesIndexItems invoiceIds={allInvoiceIds} />;
+  } else if (fetchInvoicesStatus === "rejected") {
+    content = (
+      <div>
+        <p>Error in the `InvoicesIndex` component:</p>
+        <p>the request `fetchInvoices` was rejected!</p>
+      </div>
+    );
+  } else {
+    content = (
+      <div>Something unexpected happened in the InvoicesIndex component...</div>
+    );
+  }
+
+  return <div className="InvoicesIndex">{content}</div>;
+};
+
+export default InvoicesIndex;
